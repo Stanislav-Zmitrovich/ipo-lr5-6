e17b642f29dbdb67d1c8de36df784310d7e9b622
stasik <stanislavzmitrovic3@gmail.com>
2024-12-17 21:09:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-# Открываем файл text.txt в режиме чтения
-with open('text.txt', 'r', encoding='utf-8') as file:
-    # Читаем содержимое файла
-    text = file.read()
-
-# Разделяем текст на слова
-words = text.split()
-
-# Создаем множество для хранения уникальных слов
-unique_words = set()
-
-# Добавляем каждое слово в множество уникальных слов
-for word in words:
-    unique_words.add(word)
-
-# Открываем файл output.txt в режиме записи
-with open('output.txt', 'w', encoding='utf-8') as file:
-    # Записываем каждое уникальное слово в новый файл
-    for word in unique_words:
-        file.write(word + '\n')
-
-print("Уникальные слова успешно записаны в файл output.txt")
+with open("text.txt", 'r') as file:  # Открываем файл, выдав имя file
+    text = file.read()  # Читаем строку
+words = text.split()  # Делим строку на слова, записывая в список
+unicum = list()  # Создаем новый список для уникальных слов
+for word in words:  # Для слова в словах
+    if word not in unicum:  # Если слово не в списке для уникальных слов
+        unicum.append(word) # Записываем слово в список с уникальными словами
+with open("output.txt", "w") as file:  # Открыв файл, даём ему имя file
+    file.write("Список уникальных слов из text.txt: ")  # Записываем в файл текст
+    for word in unicum:  # Для каждого слова в уникальном списке
+        file.write(word + " ")  # Записать это слово в файл
